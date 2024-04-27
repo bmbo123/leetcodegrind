2f241ca365a437a5d4944660e366991e4d19fa39
bmbo123 <101231126+bmbo123@users.noreply.github.com>
2024-04-26 20:58:46 -0500

    Time: 0 ms (100%), Space: 7.6 MB (31.44%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('|| s[i] == '[' || s[i] == '{' ){
+                stack.push(s[i]);
+            }else{
+                if(stack.empty()){
+                    return false;
+                }
+                if(s[i]== ')' && stack.top() == '('){
+                    stack.pop();
+                }else if(s[i]== ']' && stack.top() == '['){
+                    stack.pop();
+                }else if(s[i]== '}' && stack.top() == '{'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+};
