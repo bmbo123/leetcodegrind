f2132d09ca2ef72bff45056b6866572d620c9ce6
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2024-08-08 22:16:34 -0500

    Time: 52 ms (27.92%), Space: 51.1 MB (49.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxreach = 0;
+       for(int i = 0; i < nums.size(); i++){
+            if(i > maxreach ){
+                return false;
+            }
+            maxreach = max(maxreach, i+ nums[i]);
+       } 
+       return true;
+    }
+};
