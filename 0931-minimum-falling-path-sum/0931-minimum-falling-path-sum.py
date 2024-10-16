8e71b3173e5c164a7d085af97b0846fb97f08236
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2024-10-15 21:54:22 -0500

    Time: 114 ms (96.69%), Space: 17.2 MB (92.86%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -5,7 +5,7 @@ class Solution:
         dp[0] = matrix[0]
         for i in range(1,len(matrix)):
             for col in range(len(matrix)):
-                if col == n-1:
+                if col+1 >= n:
                     dp[i][col] = matrix[i][col] + min(dp[i-1][col], dp[i-1][col-1])
                 elif col == 0:
                     dp[i][col] = matrix[i][col] + min(dp[i-1][col], dp[i-1][col+1])
