24b384d00d527ea29175fb1dd9a04cf52f872684
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2025-04-06 17:37:05 -0500

    Time: 0 ms (100%), Space: 17.9 MB (9.52%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,26 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        flag = True
+
+        tes = set(digits)
+        if len(tes) == 1 and 9 in tes:
+            res = []
+            for i in range(len(digits)+1):
+                if i == 0:
+                    res.append(1)
+                else:
+                    res.append(0)
+            return res
+        
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                if flag:
+                    digits[i] = 0
+                    flag = True
+            elif flag :
+                digits[i] += 1
+                flag = False
+
+        return digits
+
+        
