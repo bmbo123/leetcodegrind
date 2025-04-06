ad605bab28666c92b8c54ded279026c5df47335c
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2025-04-06 18:56:05 -0500

    Time: 11 ms (31.97%), Space: 19.3 MB (7.64%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        pf = []
+        sf = [0] * len(nums)
+        pf.append(nums[0])
+
+        for i in range(1, len(nums)):
+            pf.append(pf[i-1]+nums[i])
+        sf[-1] = nums[-1]
+        for i in range(len(nums)-2, -1,-1):
+            sf[i] = sf[i+1] + nums[i]
+        
+        for i in range(len(nums)):
+            if sf[i] == pf[i]:
+                return i
+        
+        return -1
