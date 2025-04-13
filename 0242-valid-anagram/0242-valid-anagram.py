2ae1dd5833d87daac5e6961d46f835b30addb57d
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2025-04-13 14:38:24 -0500

    Time: 15 ms (43.54%), Space: 18.5 MB (20.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freqMap1 = {}
-        freqMap2 = {}
+        s = sorted(s)
+        t = sorted(t)
 
-        for i in range(len(s)):
-            if s[i] not in freqMap1:
-                freqMap1[s[i]] = 1
-            else:
-                freqMap1[s[i]] += 1               
-            
-        for i in range(len(t)):
-            if t[i] not in freqMap2:
-                freqMap2[t[i]] = 1
-            else:
-                freqMap2[t[i]] += 1   
-
-        return freqMap1 == freqMap2            
-            
+        return t==s
+        
