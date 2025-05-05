1a3fdb2140a12778c66661aea38c235fe07eba22
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2025-05-05 12:52:08 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        seen = {}
+        count = 0
+        for i in range(len(nums)):
+            x = nums[i]
+            x = str(x)
+            x = list(x)
+            test = reversed(x)
+            test = "".join(test)
+            test = int(test)
+            print(test)
+            r = nums[i] - test
+            if r in seen:
+                count += seen[r]
+                seen[r] += 1
+            else:
+                seen[r] = 1
+        return count
+
+            
