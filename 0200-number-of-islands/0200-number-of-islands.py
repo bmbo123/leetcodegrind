016840e08e3cb50868755bbb2d977589d10b2ff5
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2026-01-14 19:07:07 -0600

    Time: 286 ms (14.15%), Space: 34.5 MB (6.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,37 +1,50 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-
-
-        m , n = len(grid)-1, len(grid[0])-1
-        count = 0
-
-        def dfs(x, y):
-
-            if x < 0 or x > m or y > n or y < 0:
-                return
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        # visted = set()
+        # count = 0
+        # def explore(x,y):
             
-            if grid[x][y] == "1":
-                grid[x][y] = "0"
-            else:
-                return
+        #     if 1 and not in visted:
+        #         visted.add(current)
+        #         explore(top)
+        #         explore(left)
+        #         explore(right)
+        #         explore(bottom)
+             
+        # for each value:
+        #     if value not in visited and 1:
+        #         count += 1
+        #     explore(val):
             
-            dfs(x+1,y)
-            dfs(x,y+1)
-            dfs(x-1,y)
-            dfs(x, y-1)
+        # return count
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    count += 1
-                    dfs(i,j)
+        visited = set()
+        count = 0
+        
+        def explore(row,column):
+            if row < 0 or row > len(grid)-1 or column < 0 or column > len(grid[0])-1: # boundry detection come back and check this later
+                return 
+  
+            if grid[row][column] == "1" and (row,column) not in visited:
+                visited.add((row,column)) #gets specific value and adds it to the set
+                explore(row,column-1)#top  row stays,column - 1   
+                explore(row-1,column)#left row - 1  = left, column stays 
+                explore(row+1,column)# row + 1, column stays
+                explore(row,column+1)#down row stays,column + 1
+            
+        for row in range(len(grid)):
+            for column in range(len(grid[0])):
+                if (row,column) not in visited and grid[row][column] == "1":
+                    count+=1
+                explore(row,column)
+               
+
         
         return count
-
-
             
-            
-
-            
-            
+                
             
