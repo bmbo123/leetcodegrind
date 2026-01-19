6cfaeba0326cfb692848b33996ed3f7f093930bc
Alan Gregory <101231126+bmbo123@users.noreply.github.com>
2026-01-19 14:25:41 -0600

    Time: 19 ms (34.77%), Space: 21 MB (59.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #hashmap problem Freq: word
-        Anagram =  defaultdict(list)
+
+        seen = {}
+
+        for i in range(len(strs)):
+
+            norm = "".join(sorted(list(strs[i])))
+            if norm in seen:
+                seen[norm].append(strs[i])
+            else:
+                seen[norm] = [strs[i]]
         
-        for string in strs:
-            countlist = [0]*26
-            for ch in string: 
-                countlist[ord(ch) - ord('a')] +=1
-            Anagram[tuple(countlist)].append(string)
-        return list(Anagram.values())
+        res = []
+        print(seen)
+        for key in seen:
+            res.append(seen[key])
+        
+        return res
+
